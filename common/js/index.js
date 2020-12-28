8c7784b6151d43dfcc852e3bf071e08256a35b6c
Darwin Santos <ds@darwinsantos.com>
2020-12-28 19:13:38 -0400

    Tons of tinkering, kinda worked in the end.

--- common/js/index.js
+++ common/js/index.js
@@ -1,8 +1,15 @@
 $(function() {
-
-});
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=nintendo&api_key=gdwjphWDU038gRbOZBeOX0oNWQZEdPGM&limit=5");
-xhr.done(function(data)
-{ 
-   console.log(data);
+	// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=nintendo&api_key=&limit=1");
+	const apiKey = 'gdwjphWDU038gRbOZBeOX0oNWQZEdPGM'
+	fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=5`)
+	  .then(response => response.json())
+	  .then(json => {
+	    json.data
+	      .map(gif => gif.images.fixed_height.url)
+	      .forEach(url => {
+	        let img = document.createElement('img')
+	        img.src = url
+	        document.body.appendChild(img)
+	      });
+	  }).catch(error => document.body.appendChild = error);
 });
