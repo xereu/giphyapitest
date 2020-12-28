7669ef1229b9b8846c41527b7b27bd66fc33be7a
Darwin Santos <ds@darwinsantos.com>
2020-12-28 18:16:58 -0400

    Initial API Data Pull, JSON format, not an array

--- common/js/index.js
+++ common/js/index.js
@@ -1,3 +1,8 @@
 $(function() {
-	
+
+});
+var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=nintendo&api_key=gdwjphWDU038gRbOZBeOX0oNWQZEdPGM&limit=5");
+xhr.done(function(data)
+{ 
+   console.log(data);
 });
